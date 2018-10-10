47e226db88e8919340b4f13180eea2c560d2c4f0
legwohy <33128494+legwohy@users.noreply.github.com>
2018-10-11 00:28:47 +0800

    初始化

---
+++ rose-server/src/main/java/com/rose/Application.java
@@ -0,0 +1,19 @@
+package com.rose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka 服务端
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class,args);
+
+    }
+}
