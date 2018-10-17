31d22e1650e9f5e18c9cc105d64fd066dcd2caf4
leigang <lg@iyingke.cn>
2018-10-17 19:51:11 +0800

    服务客户端设置

--- rose-client/src/main/java/rose/Application.java
+++ rose-client/src/main/java/rose/Application.java
@@ -1,15 +1,21 @@
 package rose;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * eureka 客户端
  */
 @EnableEurekaClient
 @SpringBootApplication
+@RestController
 public class Application
 {
     public static void main(String[] args)
@@ -17,4 +23,18 @@ public class Application
         SpringApplication.run(Application.class,args);
 
     }
+
+    // 需要添加注解@EnableEurekaClient
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/client",method = RequestMethod.GET)
+    public String home(){
+        ServiceInstance instance=client.getLocalServiceInstance();
+
+        return "serviceId"+instance.getServiceId()+"host:post="+instance.getHost()+":"+instance.getPort();
+
+
+    }
+
 }
--- rose-server/src/main/java/com/rose/Application.java
+++ rose-server/src/main/java/com/rose/Application.java
@@ -3,17 +3,24 @@ package com.rose;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
- * eureka 服务端
+ * 开启服务注册中心
  */
 @EnableEurekaServer
 @SpringBootApplication
+@RestController
 public class Application
 {
     public static void main(String[] args)
     {
         SpringApplication.run(Application.class,args);
 
+    }
+    @RequestMapping("/server")
+    public String get(){
+        return "hello server";
     }
 }
